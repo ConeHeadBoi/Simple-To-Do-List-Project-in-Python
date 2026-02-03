520187562a7c6cdbc06868f63be7bf0a704d6bba
Esteban <estebanmunevaraquite@gmail.com>
2026-02-04 10:02:16 +1300

    Made the Menu

---
+++ app.py
@@ -0,0 +1,10 @@
+import sys
+
+def display_menu():
+	print("\nSimple To-Do List")
+	print("1. Add a task")
+	print("2. Remove a task")
+	print("3. Mark task as done/undone")
+	print("4. Display all tasks")
+	print("5. Exit")
+
